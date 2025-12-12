2323e290796ee22c12363c88d57e96e05219fdeb
emazebic <ez56227@fer.hr>
2025-12-12 15:51:39 +0100

    Update analysis.py
    Dodane funkcije za izračun SSIM (treba izabrati bolju funkciju), MS-SSIM, CIEDE2000.
    
    Co-Authored-By: Dora Zaninović <236884477+doraZanin@users.noreply.github.com>

--- functions/analysis.py
+++ functions/analysis.py
@@ -2,6 +2,9 @@ from math import log10, sqrt
 import cv2
 import numpy as np
 from pathlib import Path
+from skimage.metrics import structural_similarity as ssim
+from sewar.full_ref import msssim
+from skimage.color import deltaE_ciede2000 as ciede2000
 
 def MSE(original, compressed):
     mse = np.mean((original - compressed) ** 2)
@@ -14,6 +17,42 @@ def PSNR(original, compressed):
     max_pixel = 255.0
     psnr = 20 * log10(max_pixel / sqrt(mse))
     return psnr
+
+def SSIM(original, compressed):
+    ssim_score = ssim(original, compressed, win_size=None, channel_axis=-1)
+    return ssim_score
+
+# def SSIM(original, compressed):
+#     mean_original = np.mean(original)
+#     mean_compressed = np.mean(compressed)
+#     std_original = np.std(original)
+#     std_compressed = np.std(compressed)
+#     covariance = np.mean((original - mean_original) * (compressed - mean_compressed))
+#     c1 = (0.01 * 255) ** 2
+#     c2 = (0.03 * 255) ** 2
+#     c3 = c2 / 2
+
+#     luminance = (2 * mean_original * mean_compressed + c1) / (mean_original ** 2 + mean_compressed ** 2 + c1)
+#     contrast = (2 * std_original * std_compressed + c2) / (std_original ** 2 + std_compressed ** 2 + c2)
+#     structure = (covariance + c3) / (std_original * std_compressed + c3)
+#     ssim = luminance * contrast * structure
+#     return ssim
+
+def MS_SSIM(original, compressed):
+    ms_ssim = msssim(original, compressed)
+    return ms_ssim.real
+
+def CIEDE2000(original, compressed):
+    original = np.float32(original)
+    original *= 1./255
+    Lab1 = cv2.cvtColor(original, cv2.COLOR_BGR2Lab)
+
+    compressed = np.float32(compressed)
+    compressed *= 1./255
+    Lab2 = cv2.cvtColor(compressed, cv2.COLOR_BGR2Lab)
+
+    c2000 = ciede2000(Lab1, Lab2, channel_axis=-1)
+    return c2000
 
 
 def run_analysis (original_folder, reconstructed_folder, analysis_output_folder, method):
@@ -30,7 +69,13 @@ def run_analysis (original_folder, reconstructed_folder, analysis_output_folder,
         value = MSE(image, compressed)
         f.write("\n MSE: " + str(value))
         value = PSNR(image, compressed)
-        f.write("\n PSNR: " + str(value) + " dB\n")
-        # ...
+        f.write("\n PSNR: " + str(value) + " dB")
+        value = SSIM(image, compressed)
+        f.write("\n SSIM: " + str(value))
+        value = MS_SSIM(image, compressed)
+        f.write("\n MS_SSIM: " + str(value))
+        value = CIEDE2000(image, compressed)
+        f.write("\n CIEDE2000: " + str(value) + "\n\n")
+
     f.close()
     return 
